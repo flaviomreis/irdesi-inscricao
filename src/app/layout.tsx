d05b7d6cb5947dceb8514f7e987f30f787fed539
flaviomreis <flaviomreis@gmail.com>
2023-07-14 15:06:33 -0300

    alterada fonte para Amaranth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Amaranth, Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const appFont = Amaranth({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Irdesi - Pré-Inscrição",
@@ -16,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={appFont.className}>{children}</body>
     </html>
   );
 }
