1232a6225dadf63811deddad41b38e97536dd2ff
flaviomreis <flaviomreis@gmail.com>
2023-07-15 00:27:20 -0300

    ajuste na página admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,18 +1,13 @@
 import { prisma } from "@/db/connection";
 import { Metadata } from "next";
 import NextAuthProvider from "../providers/auth";
-import UserAuth from "@/components/UserAuth";
+import UserAuthAction from "@/components/UserAuthAction";
 import { useState } from "react";
 
 export const metadata: Metadata = {
   title: "Irdesi - Administração",
   description: "Administração de Pré-Inscrições",
 };
-
-async function getAdministrators() {
-  const administrators = await prisma.administrator.findMany({});
-  return administrators;
-}
 
 async function getCourses() {
   const courses = await prisma.course.findMany({});
@@ -30,47 +25,38 @@ async function getCourseClasses() {
 }
 
 export default async function AdminPage() {
-  const administrators = await getAdministrators();
   const courses = await getCourses();
   const courseClasses = await getCourseClasses();
 
   return (
     <NextAuthProvider>
-      <UserAuth />
-      <h2>Administradores</h2>
-      {administrators.map((item) => {
-        return (
-          <p key={item.id}>
-            {item.name}
-            {" : "}
-            {item.email}
-          </p>
-        );
-      })}
-      <h2>Cursos</h2>
-      {courses.map((item) => {
-        return (
-          <p key={item.id}>
-            {item.name}
-            {" : "}
-            {item.short_name}
-          </p>
-        );
-      })}
-      <h2>Turmas</h2>
-      {courseClasses.map((item) => {
-        return (
-          <p key={item.id}>
-            {item.course.name}
-            {" : "}
-            {item.course.short_name}
-            {" / "}
-            {item.institution.name}
-            {" : "}
-            {item.institution.short_name}
-          </p>
-        );
-      })}
+      <div className="container mx-auto p-4">
+        <UserAuthAction />
+        <h2>Cursos</h2>
+        {courses.map((item) => {
+          return (
+            <p key={item.id}>
+              {item.name}
+              {" : "}
+              {item.short_name}
+            </p>
+          );
+        })}
+        <h2>Turmas</h2>
+        {courseClasses.map((item) => {
+          return (
+            <p key={item.id}>
+              {item.course.name}
+              {" : "}
+              {item.course.short_name}
+              {" / "}
+              {item.institution.name}
+              {" : "}
+              {item.institution.short_name}
+            </p>
+          );
+        })}
+      </div>
     </NextAuthProvider>
   );
 }
---
+++ src/components/UserAuthAction.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function UserAuthAction() {
+  const { data, status } = useSession();
+  return (
+    <div className="flex justify-between items-center border-b border-gray-400">
+      {status === "unauthenticated" ? (
+        <div className="flex items-center justify-between w-full">
+          <div>Área do Administrador</div>
+          <button
+            onClick={() => signIn()}
+            className="bg-purple-800 font-bold text-sm rounded text-white p-2 px-4 hover:bg-purple-600"
+          >
+            Entrar
+          </button>
+        </div>
+      ) : (
+        <div className="flex items-center justify-between w-full">
+          <div className="flex items-center gap-2">
+            Área do Administrador:
+            <span className="text-purple-800">{data?.user?.name}</span>
+            <img
+              className="rounded-full"
+              src={data?.user?.image ?? ""}
+              width={32}
+            />
+          </div>
+          <button
+            onClick={() => signOut()}
+            className="bg-purple-800 font-bold text-sm rounded text-white p-2 px-4 hover:bg-purple-600"
+          >
+            Sair
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
