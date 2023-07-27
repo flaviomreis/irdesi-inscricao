b1f50bcd0bf7ab581a4a726f40b2bec58d78531a
flaviomreis <flaviomreis@gmail.com>
2023-07-27 20:36:38 -0300

    substitui button e useRouter por Link

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,31 +1,32 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { useState } from "react";
 
 type Props = {
   courseClassId: string;
 };
 
 export default function DownloadButton({ courseClassId }: Props) {
-  const router = useRouter();
-
-  async function handleClick() {
-    const checked = (document.getElementById("checked") as HTMLInputElement)
-      .checked;
-    console.log(checked);
-    router.push(`/api/download/${courseClassId}?checked=${checked}`);
-  }
+  const [zip, setZip] = useState(false);
+  const [groups, setGroups] = useState(100);
 
   return (
     <div className="flex items-center">
-      <button
-        onClick={handleClick}
+      <Link
+        href={`/api/download/${courseClassId}?checked=${zip}&groups=${groups}`}
+        target="_blank"
         className="flex items-center justify-center flex-1 bg-purple-800 text-sm rounded font-bold text-white h-10 hover:bg-purple-600"
       >
         Download
-      </button>
+      </Link>
       <label className="px-4">
-        <input className="mr-2" type="checkbox" id="checked" />
+        <input
+          className="mr-2"
+          type="checkbox"
+          checked={zip}
+          onChange={(e) => setZip(!zip)}
+        />
         Zip
       </label>
       <label className="text-xs">
@@ -33,7 +34,8 @@ export default function DownloadButton({ courseClassId }: Props) {
         <input
           className="text-xs w-20 border-0"
           type="number"
-          value="100"
+          value={groups}
+          onChange={(e) => setGroups(+e.target.value)}
           step="100"
         />
       </label>
